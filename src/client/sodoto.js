273d26562caf305e6d91fad90575c53c6297e091
Ward Cunningham <ward@c2.com>
2025-06-09 12:38:10 -0700

    remember and report achievements

--- src/client/sodoto.js
+++ src/client/sodoto.js
@@ -9,38 +9,55 @@ const localize = ($item,item) => {
   const $page = $item.parents('.page')
   const owner = $page.data('site') || location.host
   const page = $page.data('data')
+  const lesson = item.text.trim().split(/\n/)[0].trim()
+  if ('forward' in item) delete item.forward
+  console.log({lesson,owner,item,page})
   if (item.title != page.title) {
     item.title = page.title
-    item.forward = []
+    item.students = {}
   }
-  const lesson = item.text.trim()
   return {$item, item, owner, page, lesson}  
 }
 
-const score = (site,selected) => {
+const score = (site,assessment) => {
   return `<span data-site="${site}">
-    <input type=checkbox name=see>
+    <input type=checkbox name=see ${assessment.see ? 'checked' : ''}>
     <label for=see> seen </label>
-    <input type=checkbox name=do>
+    <input type=checkbox name=do ${assessment.do ? 'checked' : ''}>
     <label for=do> done </label>
-    <input type=checkbox name=teach>
+    <input type=checkbox name=teach ${assessment.teach ? 'checked' : ''}>
     <label for=teach> taught </label>
   </span>`
 }
 
-const todo = context => {
+const doing = context => {
+  const slug = wiki.asSlug(context.page.title)
+  const have = Object.entries(context.item?.students ?? {})
+  const details = have.map(([site, assessment]) => `
+    &nbsp; <img class='remote' src='${wiki.site(site).flag()}'
+    title='${site}' data-site="${site}" data-slug="${slug}">
+    ${score(site,assessment)}`).join("<br>")
+  return have.length
+    ? `<details><summary>${have.length} students</summary>
+      ${details}
+      </details>`
+    : ""
+}
+
+const cando = context => {
   const slug = wiki.asSlug(context.page.title)
   const have = Object.entries(wiki.neighborhoodObject.sites)
     .filter(([domain, site]) => !site.sitemapRequestInflight && site.sitemap)
     .filter(([domain, site]) => site.sitemap.find(info => info.slug == slug))
     .map(([domain, site]) => domain)
     .filter(domain => domain != context.owner)
+    .filter(domain => !(domain in (context.item?.students ?? {})))
   const details = have.map(site => `
     &nbsp; <img class='remote' src='${wiki.site(site).flag()}'
     title='${site}' data-site="${site}" data-slug="${slug}">
-    ${score(site,[])}`).join("<br>")
+    ${score(site,context.item?.students?.[site] ?? {} )}`).join("<br>")
   return have.length
-    ? `<details><summary>${have.length} students</summary>
+    ? `<details><summary>${have.length} others</summary>
       ${details}
       </details>`
     : ""
@@ -51,12 +68,12 @@ const emit = ($item, item) => {
   const teacher = context.teacher
   let confs = null
   if(teacher) confs = wiki.site(teacher).get(asSlug(item.title)).story.find(each => each.id==item.id)
-  console.log({context,confs})
   return $item.append(`
     <div style="background-color:#eee;padding:15px;">
       <b>${expand(context.lesson)}</b><br>
-      ${score('me',[])}
-      ${todo(context)}
+      ${score('me',item.assessment ?? {})}
+      ${doing(context)}
+      ${cando(context)}
     </div>`)
 }
 
@@ -68,7 +85,6 @@ const bind = ($item, item) => {
   $('body').on('new-neighbor-done', (e, site) => {
     $item.empty()
     emit($item, item)
-    bind($item,item)
   })
   $item.on('change', event => {
     event.preventDefault()
@@ -80,7 +96,19 @@ const bind = ($item, item) => {
     // const site = target.closest('span').dataset.site
     const span = target.closest('span')
     const site = span.dataset.site
-    console.log({event,target,name,active,disabled,span,site})
+    if(site=='me') {
+      if(!('assessment' in item)) item.assessment = {}
+      item.assessment[name] = active
+    } else {
+      if(!('students' in item)) item.students = {}
+      if(!(site in item.students)) item.students[site] = {}
+      item.students[site][name] = active
+    }
+    wiki.pageHandler.put($item.parents('.page:first'), {
+      type: 'edit',
+      id: item.id,
+      item: item,
+    })
     return true
   })
 }
