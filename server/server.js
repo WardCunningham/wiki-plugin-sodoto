246a34c0bffd1f8d7bb4a83853b3b5188f471751
Ward Cunningham <ward@c2.com>
2025-07-17 09:55:49 -0700

    pivot to delegate more to bdo

---
+++ server/server.js
@@ -0,0 +1,64 @@
+// register plugin, server-side component
+// These handlers are launched with the wiki server.
+
+import fs from 'node:fs'
+import fsp from 'node:fs/promises'
+import path from 'node:path'
+import bdo from 'bdo-js'
+
+
+const startServer = async function (params) {
+  const { app, argv } = params
+
+  // settings = null
+  // fs.readFile path.resolve(argv.status, 'plugins', 'register', 'settings.json'), (err, text) ->
+  //   return console.log('register settings', err) if err
+  //   settings = JSON.parse text
+
+  const admin = function (req, res, next) {
+    if (app.securityhandler.isAdmin(req)) {
+      return next()
+    } else {
+      let a, u
+      if (!argv.admin) {
+        a = 'no admin specified'
+      }
+      if (!req.session?.passport?.user && !req.session?.email && !req.session?.friend) {
+        u = 'not logged in'
+      }
+      return res.status(403).send(`Must be admin user, ${a || u}`)
+    }
+  }
+
+  const farm = function (req, res, next) {
+    if (argv.farm) {
+      return next()
+    } else {
+      return res.status(403).send({ error: 'Must be wiki farm to make subdomains' })
+    }
+  }
+
+  const owner = function (req, res, next) {
+    if (!app.securityhandler.isAuthorized(req)) {
+      return res.status(401).send('must be owner')
+    }
+    return next()
+  }
+
+
+  app.get('/plugin/sodoto/view', farm, owner, async function (req, res) {
+    const e500 = msg => res.status(500).send(msg)
+
+    const hash = ``
+    const newBDO = {}
+    let keys;
+    const saveKeys = (k) => keys = k;
+    const getKeys = () => keys;
+    const uuid = await bdo.createUser(hash, newBDO, saveKeys, getKeys);
+    const sodotoBDO = await bdo.getBDO(uuid, hash, '0211749ad18cc9ca022ce8bf101b4e8769461dcdc399e65d0fb78625047d36f15a');
+    return res.json(sodotoBDO)
+  })
+
+}
+
+export { startServer }
--- src/client/sodoto.js
+++ src/client/sodoto.js
@@ -5,116 +5,22 @@ const expand = text => {
     .replace(/>/g, '&gt;')
 }
 
-const localize = ($item,item) => {
-  const $page = $item.parents('.page')
-  const owner = $page.data('site') || location.host
-  const page = $page.data('data')
-  const lesson = item.text.trim().split(/\n/)[0].trim()
-  if ('forward' in item) delete item.forward
-  console.log({lesson,owner,item,page})
-  if (item.title != page.title) {
-    item.title = page.title
-    item.students = {}
-  }
-  return {$item, item, owner, page, lesson}  
-}
+const emit = async ($item, item) => {
 
-const score = (site,assessment) => {
-  return `<span data-site="${site}">
-    <input type=checkbox name=see ${assessment.see ? 'checked' : ''}>
-    <label for=see> seen </label>
-    <input type=checkbox name=do ${assessment.do ? 'checked' : ''}>
-    <label for=do> done </label>
-    <input type=checkbox name=teach ${assessment.teach ? 'checked' : ''}>
-    <label for=teach> taught </label>
-  </span>`
-}
-
-const doing = context => {
-  const slug = wiki.asSlug(context.page.title)
-  const have = Object.entries(context.item?.students ?? {})
-  const details = have.map(([site, assessment]) => `
-    &nbsp; <img class='remote' src='${wiki.site(site).flag()}'
-    title='${site}' data-site="${site}" data-slug="${slug}">
-    ${score(site,assessment)}`).join("<br>")
-  return have.length
-    ? `<details><summary>${have.length} students</summary>
-      ${details}
-      </details>`
-    : ""
-}
-
-const cando = context => {
-  const slug = wiki.asSlug(context.page.title)
-  const have = Object.entries(wiki.neighborhoodObject.sites)
-    .filter(([domain, site]) => !site.sitemapRequestInflight && site.sitemap)
-    .filter(([domain, site]) => site.sitemap.find(info => info.slug == slug))
-    .map(([domain, site]) => domain)
-    .filter(domain => domain != context.owner)
-    .filter(domain => !(domain in (context.item?.students ?? {})))
-  const details = have.map(site => `
-    &nbsp; <img class='remote' src='${wiki.site(site).flag()}'
-    title='${site}' data-site="${site}" data-slug="${slug}">
-    ${score(site,context.item?.students?.[site] ?? {} )}`).join("<br>")
-  return have.length
-    ? `<details><summary>${have.length} others</summary>
-      ${details}
-      </details>`
-    : ""
-}
-
-const emit = ($item, item) => {
-  const context = localize($item,item)
-  const teacher = context.teacher
-  let confs = null
-  if(teacher) confs = wiki.site(teacher).get(asSlug(item.title)).story.find(each => each.id==item.id)
+  const sodotoBDO = await fetch(`/plugin/sodoto/view`).then(res => res.json())
   return $item.append(`
     <div style="background-color:#eee;padding:15px;">
-      <b>${expand(context.lesson)}</b><br>
-      ${score('me',item.assessment ?? {})}
-      ${doing(context)}
-      ${cando(context)}
+      ${sodotoBDO.bdo.svg}
     </div>`)
 }
 
 const bind = ($item, item) => {
   $item.dblclick(() => {
-    item = localize($item,item).item
     return wiki.textEditor($item, item)
-  })
-  $('body').on('new-neighbor-done', (e, site) => {
-    $item.empty()
-    emit($item, item)
-  })
-  $item.on('change', event => {
-    event.preventDefault()
-    event.stopPropagation()
-    const target = event.target
-    const name = target.name
-    const active = target.checked
-    const disabled = target.disabled
-    // const site = target.closest('span').dataset.site
-    const span = target.closest('span')
-    const site = span.dataset.site
-    if(site=='me') {
-      if(!('assessment' in item)) item.assessment = {}
-      item.assessment[name] = active
-    } else {
-      if(!('students' in item)) item.students = {}
-      if(!(site in item.students)) item.students[site] = {}
-      item.students[site][name] = active
-    }
-    wiki.pageHandler.put($item.parents('.page:first'), {
-      type: 'edit',
-      id: item.id,
-      item: item,
-    })
-    return true
   })
 }
 
 if (typeof window !== 'undefined') {
   window.plugins.sodoto = { emit, bind }
 }
-
 export const sodoto = typeof window == 'undefined' ? { expand } : undefined
